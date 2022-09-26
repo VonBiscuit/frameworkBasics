6f11916e300f39adc1d6705453cce27c48eb5347
VonBiscuit <oryon_brew@yahoo.com>
2022-09-26 16:41:02 +0300

    refactored conftest.py

--- conftest.py
+++ conftest.py
@@ -5,22 +5,11 @@ import pytest
 @pytest.fixture(scope="function", autouse=True)
 def cleanup_created_objects():
 
-    if ou.User.user_instances:
-        local_user_list = [user for user in ou.User.user_instances]
-        for user in local_user_list:
-            user.delete_object()
+    object_list = []
+    object_list += ou.User.user_instances
+    object_list += ou.Post.post_instances
+    object_list += ou.Comment.comment_instances
+    object_list += ou.Todo.todo_instances
 
-    if ou.Post.post_instances:
-        local_post_list = [post for post in ou.Post.post_instances]
-        for post in local_post_list:
-            post.delete_object()
-
-    if ou.Comment.comment_instances:
-        local_comment_list = [comment for comment in ou.Comment.comment_instances]
-        for comment in local_comment_list:
-            comment.delete_object()
-
-    if ou.Todo.todo_instances:
-        local_todo_list = [todo for todo in ou.Todo.todo_instances]
-        for todo in local_todo_list:
-            todo.delete_object()
+    for api_object in object_list:
+        api_object.delete_object()
